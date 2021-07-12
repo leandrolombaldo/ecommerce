079fce47e591c9aba8aa78768a1ccdf819f55ec1
Leandro da silva <leandrosilva.hair@gmail.com>
2021-07-12 09:04:06 -0300

    modified styles navbar

---
+++ src/components/Navbar/styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    
+  .navbar {
+     width: 100%;
+     position: absolute;
+     background: transparent;
+
+     @media screen and (max-width: 768px) {
+        position: relative;
+        background: #AC7C83;
+     }
+  }
+
+`;
