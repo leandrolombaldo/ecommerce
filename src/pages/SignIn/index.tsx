77e3ccfbc0a0d5b6adb9e3bb83f70ce5fd1347da
Leandro da silva <leandrosilva.hair@gmail.com>
2021-08-20 12:22:11 -0300

    modified file buttons and inputs

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,33 +1,75 @@
-import { LoginTotal, Img, Form } from "./styles";
+import {useCallback, useRef} from 'react';
 
-import backLogin from "../../assets/backgroundLogin.svg";
+import { Container, Content, Background } from "./styles";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import { FormHandles } from '@unform/core';
+import * as Yup from "yup";
 
-import Navtop from "../../components/Navbar";
-import Footer from "../../components/NavFooter";
 
-const Login = () => {
+import logoImg from "../../assets/logo.svg";
 
-  
+import Navbar from '../../components/Navbar';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+
+
+function SignIn() {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      })
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      
+    } catch (err) {
+      console.log(err);
+
+      const errors = getValidationErrors(err);
+      
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <>
-      <Navtop />
-      <LoginTotal>
-        <Img>
-          <img src={backLogin} alt="img login" />
-        </Img>
-        <Form>
-          <h2>Login</h2>
-          <label>Email</label>
-          <input type="text" id="email" name="email" />
-          <label>Password</label>
-          <input type="text" id="password" name="password" />
-          <button>Login</button>
+    <Navbar />
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Store Carolina's" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Password" />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="#">Esqueci minha senha</a>
         </Form>
-      </LoginTotal>
-      <Footer />
+        <a href="/signUp">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
     </>
   );
-};
+}
 
-export default Login;
+export default SignIn;
